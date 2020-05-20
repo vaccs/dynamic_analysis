8db861a22e1e3d05fc99df584c213ec6a372f1af
Steve Carr <steve.carr@wmich.edu>
2020-05-20 18:08:51 -0400

    edited format of error messages to work better with eclipse plugin

--- vaccs_pin_cpp/libs/util/vaccs_error_message.h
+++ vaccs_pin_cpp/libs/util/vaccs_error_message.h
@@ -10,6 +10,50 @@ using namespace std;
 #define DELIMITER ":"
 #define VACCS_ERROR_LEVEL_WARNING "warning"
 #define VACCS_ERROR_LEVEL_ERROR "error"
+#define VACCS_ERROR_LEVEL_INFO "info"
+
+#define ACCESS_FAULT_ID "accessFault"
+#define ACCESS_MSG "invalid memory access"
+#define ILL_FAULT_ID "illFault"
+#define ILL_MSG "illegal instruction"
+#define STK_FAULT_ID "stkFault"
+#define STK_MSG "corrupted stack detected"
+#define RBP_FAULT_ID "rbpFault"
+#define RBP_MSG "stack smashing of old frame pointer detected"
+#define RA_FAULT_ID "raFault"
+#define RA_MSG "stack smashing of return address detected"
+#define PROT_FAULT_ID "protFault"
+#define PROT_MSG "memory protection violation"
+#define PAGE_FAULT_ID "pageFault"
+#define PAGE_MSG "invalid memory page"
+#define ALIGN_FAULT_ID "alignFault"
+#define ALIGN_MSG "invalid memory page"
+#define PRIV_FAULT_ID "privFault"
+#define PRIV_MSG "invalid memory page"
+#define DIV_FAULT_ID "divFault"
+#define DIV_MSG "integer divide by zero"
+#define OVER_FAULT_ID "overFault"
+#define OVER_MSG "integer overflow"
+#define BND_FAUlT_ID "bndFault"
+#define BND_MSG "array out-of-bounds"
+#define FDIV_FAULT_ID "fdivFault" 
+#define FDIV_MSG "floating-point divide by zero"
+#define FOVER_FAULT_ID "foverFault"
+#define FOVER_MSG "floating-point overflow"
+#define FUNDR_FAULT_ID "fundrFault"
+#define FUNDR_MSG "floating-point underflow"
+#define FEXT_FAULT_ID "fextFault"
+#define FEXT_MSG "floating-point inexact result"
+#define FINV_FAULT_ID "finvFault"
+#define FINV_MSG "floating-point invalid operation"
+#define FNRM_FAULT_ID "fnrmFault"
+#define FNRM_MSG "floating-point denormalized operand"
+#define FSTK_FAULT_ID "fstkFault"
+#define FSTK_MSG "floating-point stack error"
+#define FUNK_FAULT_ID "funkFault"
+#define FUNK_MSG "floating-point unknown error"
+#define UNK_FAULT_ID "unkFault"
+#define UNK_MSG "unknow error"
 
 class vaccs_error_message
 {
@@ -18,6 +62,7 @@ private:
 	int line;
 	string severity;
 	string message;
+	string id;
 
 public:
 	vaccs_error_message();
@@ -67,8 +112,19 @@ public:
 		return this;
 	}
 
+	string get_id() const
+	{
+		return id;
+	}
+
+	vaccs_error_message* add_id(string id)
+	{
+		this->id = id;
+		return this;
+	}
+
 	string get_vaccs_formatted_error_message() {
-		return "["+file_name + DELIMITER + decstr(line) + "]" + DELIMITER + " (" + severity + ") " + message;
+		return file_name + DELIMITER + decstr(line) + DELIMITER + message + DELIMITER + " (" + id + ") ";
 	}
 
 	void emit_vaccs_formatted_error_message() {
